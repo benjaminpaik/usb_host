3fc0e7ee7391f3b03a0bb7d70087e1fe6d640832
bpaik <benjaminpaik@gmail.com>
2023-02-03 14:19:09 -0800

    Changed to menubar to allow shortcuts and updated menubar dimensions.

--- lib/widgets/topbar.dart
+++ lib/widgets/topbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:usb_host/misc/file_utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -5,6 +6,9 @@ import 'package:provider/provider.dart';
 import '../models/host_data_model.dart';
 import '../models/parameter_table_model.dart';
 import 'message_widget.dart';
+
+const _verticalPadding = 8.0;
+const _horizontalPadding = 8.0;
 
 class TopBar extends StatelessWidget {
   const TopBar({Key? key}) : super(key: key);
@@ -15,68 +19,71 @@ class TopBar extends StatelessWidget {
     final parameterTableModel =
     Provider.of<ParameterTableModel>(context, listen: false);
 
-    final openFileMenuItem = PopupMenuItem(
-        child: const Text("open file"),
-        onTap: () {
+    final openFileMenuItem = MenuItemButton(
+        onPressed: () {
           hostDataModel.openConfigFile(() {
             if (hostDataModel.configData.initialized) {
-              parameterTableModel.initNumParameters(
-                  hostDataModel.configData.parameter.length);
+              parameterTableModel
+                  .initNumParameters(hostDataModel.configData.parameter.length);
             }
             displayMessage(context, hostDataModel.userMessage);
           });
-        });
+        },
+        shortcut: const SingleActivator(LogicalKeyboardKey.keyO, control: true),
+        child: const Text("open file"));
 
-    final saveFileMenuItem = PopupMenuItem(
-        child: Selector<HostDataModel, bool>(
-          selector: (_, selectorModel) =>
-          selectorModel.configData.telemetry.isNotEmpty,
-          builder: (context, fileLoaded, child) {
-            return Text(
-              "save file",
-              style: TextStyle(
-                color: fileLoaded ? null : Colors.grey,
-              ),
-            );
-          },
-        ),
-        onTap: () {
-          if (hostDataModel.configData.telemetry.isNotEmpty) {
-            hostDataModel
-                .saveFile(generateConfigFile(hostDataModel.configData));
-          }
-        });
+    final saveFileMenuItem = MenuItemButton(
+      onPressed: () {
+        if (hostDataModel.configData.telemetry.isNotEmpty) {
+          hostDataModel.saveFile(generateConfigFile(hostDataModel.configData));
+        }
+      },
+      shortcut: const SingleActivator(LogicalKeyboardKey.keyS, control: true),
+      child: Selector<HostDataModel, bool>(
+        selector: (_, selectorModel) =>
+        selectorModel.configData.telemetry.isNotEmpty,
+        builder: (context, fileLoaded, child) {
+          return Text(
+            "save file",
+            style: TextStyle(
+              color: fileLoaded ? null : Colors.grey,
+            ),
+          );
+        },
+      ),
+    );
 
-    final createDataFileMenuItem = PopupMenuItem(
-        child: Selector<HostDataModel, bool>(
-          selector: (_, selectorModel) => selectorModel.usb.isRunning,
-          builder: (context, running, child) {
-            return Text(
-              "create data file",
-              style: TextStyle(
-                color: running ? null : Colors.grey,
-              ),
-            );
-          },
-        ),
-        onTap: () {
-          if (hostDataModel.usb.isRunning) {
-            hostDataModel.createDataFile();
-          }
-        });
+    final createDataFileMenuItem = MenuItemButton(
+      onPressed: () {
+        if (hostDataModel.usb.isRunning) {
+          hostDataModel.createDataFile();
+        }
+      },
+      shortcut: const SingleActivator(LogicalKeyboardKey.keyD, control: true),
+      child: Selector<HostDataModel, bool>(
+        selector: (_, selectorModel) => selectorModel.usb.isRunning,
+        builder: (context, running, child) {
+          return Text(
+            "create data file",
+            style: TextStyle(
+              color: running ? null : Colors.grey,
+            ),
+          );
+        },
+      ),
+    );
 
-    final parseDataMenuItem = PopupMenuItem(
+    final parseDataMenuItem = MenuItemButton(
         child: const Text("parse data"),
-        onTap: () {
+        onPressed: () {
           hostDataModel.parseDataFile(
               true, () => displayMessage(context, hostDataModel.userMessage));
         });
 
-    final saveByteFileMenuItem = PopupMenuItem(
+    final saveByteFileMenuItem = MenuItemButton(
       child: Row(
         children: [
           const Text("save byte file"),
-          const Spacer(),
           Selector<HostDataModel, bool>(
             selector: (_, selectorModel) => selectorModel.saveByteFile,
             builder: (context, saveByteFile, child) {
@@ -89,75 +96,70 @@ class TopBar extends StatelessWidget {
           ),
         ],
       ),
+      onPressed: () {},
     );
 
-    final createHeaderMenuItem = PopupMenuItem(
-        child: const Text("create header"),
-        onTap: () {
-          if (hostDataModel.configData.telemetry.isNotEmpty) {
-            hostDataModel
-                .saveFile(generateHeaderFile(hostDataModel.configData));
-          }
-        });
-
-    final programTargetMenuItem = PopupMenuItem(
-        child: const Text("program target"),
-        onTap: () {
-          hostDataModel.initBootloader().then((_) {
-            displayMessage(context, hostDataModel.userMessage);
-            hostDataModel.usb.connect();
-          });
-        });
-
-    final fileMenu = Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: PopupMenuButton(
-        child: const Text("File"),
-        itemBuilder: (context) => <PopupMenuItem>[
-          openFileMenuItem,
-          saveFileMenuItem,
-          createDataFileMenuItem,
-        ],
-      ),
-    );
-
-    final toolsMenu = Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: PopupMenuButton(
-        child: const Text("Tools"),
-        itemBuilder: (context) => <PopupMenuItem>[
-          parseDataMenuItem,
-          saveByteFileMenuItem,
-          createHeaderMenuItem,
-          programTargetMenuItem,
-        ],
-      ),
-    );
-
-    const comPortLabel = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text("Port: "),
-    );
-
-    final recordButton = Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Selector<HostDataModel, RecordState>(
-        selector: (_, selectorModel) => selectorModel.usb.recordState,
-        builder: (context, recordState, child) {
-          return IconButton(
-              onPressed: () {
-                hostDataModel.recordButtonEvent(() {
-                  displayMessage(context, hostDataModel.userMessage);
-                });
-              },
-              icon: recordState.icon);
+    final createHeaderMenuItem = MenuItemButton(
+      onPressed: () {
+        if (hostDataModel.configData.telemetry.isNotEmpty) {
+          hostDataModel.saveFile(generateHeaderFile(hostDataModel.configData));
+        }
+      },
+      shortcut: const SingleActivator(LogicalKeyboardKey.keyH, control: true),
+      child: Selector<HostDataModel, bool>(
+        selector: (_, selectorModel) =>
+        selectorModel.configData.telemetry.isNotEmpty,
+        builder: (context, fileLoaded, child) {
+          return Text(
+            "create header",
+            style: TextStyle(
+              color: fileLoaded ? null : Colors.grey,
+            ),
+          );
         },
       ),
     );
 
+    final programTargetMenuItem = MenuItemButton(
+      onPressed: () {
+        hostDataModel.initBootloader().then((_) {
+          displayMessage(context, hostDataModel.userMessage);
+          hostDataModel.usb.closePort();
+        });
+      },
+      shortcut: const SingleActivator(LogicalKeyboardKey.keyP, control: true),
+      child: const Text("program target"),
+    );
+
+    final fileMenu = [
+      openFileMenuItem,
+      saveFileMenuItem,
+      createDataFileMenuItem,
+    ];
+
+    final toolsMenu = [
+      parseDataMenuItem,
+      saveByteFileMenuItem,
+      createHeaderMenuItem,
+      programTargetMenuItem,
+    ];
+
+    final recordButton = Selector<HostDataModel, RecordState>(
+      selector: (_, selectorModel) => selectorModel.usb.recordState,
+      builder: (context, recordState, child) {
+        return IconButton(
+            onPressed: () {
+              hostDataModel.recordButtonEvent(() {
+                displayMessage(context, hostDataModel.userMessage);
+              });
+            },
+            icon: recordState.icon);
+      },
+    );
 
     final connectButton = Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(
+          vertical: _verticalPadding, horizontal: _horizontalPadding),
       child: SizedBox(
         width: 110.0,
         child: ElevatedButton(
@@ -179,15 +181,31 @@ class TopBar extends StatelessWidget {
       ),
     );
 
+    // combine items from both menus and register shortcuts
+    _initShortcuts(context, [...fileMenu, ...toolsMenu]);
+
     return Row(
       children: [
-        fileMenu,
-        toolsMenu,
-        recordButton,
+        MenuBar(children: [
+          SubmenuButton(menuChildren: fileMenu, child: const Text('File')),
+          SubmenuButton(menuChildren: toolsMenu, child: const Text('Tools')),
+          recordButton,
+        ]),
         const Spacer(),
-        comPortLabel,
         connectButton,
       ],
     );
+  }
+}
+
+void _initShortcuts(BuildContext context, List<MenuItemButton> menuItems) {
+  if (ShortcutRegistry.of(context).shortcuts.isEmpty) {
+    final validMenuItems = menuItems
+        .where((item) => item.shortcut != null && item.onPressed != null);
+    final shortcutMap = {
+      for (final item in validMenuItems)
+        item.shortcut!: VoidCallbackIntent(item.onPressed!)
+    };
+    ShortcutRegistry.of(context).addAll(shortcutMap);
   }
 }
